c43a8c695e282700aa252eb2b634a85be6bc7a64
anetfreak <anetfreak@gmail.com>
2014-03-09 16:52:44 -0700

    ignoring an import..

--- src/poke/server/management/managers/HeartbeatConnector.java
+++ src/poke/server/management/managers/HeartbeatConnector.java
@@ -22,7 +22,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import poke.monitor.HeartMonitor;
-import poke.monitor.MonitorHandler;
 import poke.server.management.managers.HeartbeatData.BeatStatus;
 
 /**
