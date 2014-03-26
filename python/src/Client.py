da7698b6d7f6366dd2e5e29bb58d89d7ba040332
anetfreak <anetfreak@gmail.com>
2014-03-26 00:56:44 -0700

    Formed the request object but the request sending fails..

--- python/src/Client.py
+++ python/src/Client.py
@@ -1,5 +1,5 @@
 from SocketChannel import SocketChannel, SocketChannelFactory
-from comm_pb2 import Request
+from comm_pb2 import Request, Header, Payload, RoutingPath
 
 class Client():
   def __init__(self):
@@ -10,5 +10,13 @@ class Client():
 	while self.channel.connected:
 	  print "Channel Connected..."
 	  request = Request()
-	  print request
-	  break
+	  header = request.header
+	  body = request.body
+	  
+	  header.routing_id = 4
+	  header.originator = "zero"
+	  job_op = body.job_op
+	  job_op.action = 4
+	  
+	  self.channel.write(request.SerializeToString())
+	  
