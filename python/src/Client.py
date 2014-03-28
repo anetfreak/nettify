b2caa19c008fdc4c48a10273e7d4fbd2dbdcd648
anetfreak <anetfreak@gmail.com>
2014-03-27 17:10:22 -0700

    Adding Python files and specific python related files..

--- python/src/Client.py
+++ python/src/Client.py
@@ -1,6 +1,8 @@
 import sys
+
 sys.path.append("netty-all-4.0.15.Final.jar")
 sys.path.append("protobuf-java-2.5.0.jar")
+
 from eye.Comm import Request, Header, Payload, RoutingPath
 from io.netty.bootstrap import Bootstrap, ChannelFactory
 from io.netty.buffer import PooledByteBufAllocator, Unpooled
@@ -8,42 +10,37 @@ from io.netty.channel import ChannelInboundHandlerAdapter, ChannelInitializer, C
 from io.netty.channel.nio import NioEventLoopGroup
 from io.netty.channel.socket.nio import NioSocketChannel
 from io.netty.handler.ssl import SslHandler
+from javax.net.ssl import SSLContext
 
+class SSLInitializer(ChannelInitializer):
+ 
+  def initChannel(self, ch):
+    pipeline = ch.pipeline()
+    engine = SSLContext.getDefault().createSSLEngine()
+    engine.setUseClientMode(True);
+    pipeline.addLast("ssl", SslHandler(engine))
+        
 class Client():
-#  def __init__(self):
-#    self.channelFactory = SocketChannelFactory()
-#	
-#  def run(self, host, port):
-#	self.channel = self.channelFactory.openChannel(host, port)
-#	while self.channel.connected:
-#	  print "Channel Connected..."
-#	  request = Request()
-#	  header = request.header
-#	  body = request.body
-#	  
-#	  header.routing_id = 4
-#	  header.originator = "zero"
-#	  job_op = body.job_op
-#	  job_op.action = 4
-#	  
-#	  self.channel.write(request.SerializeToString())
-#	  print "Writing " + request.SerializeToString() + " to queue"
-#	  continue
 
   def run(self, host, port):
   
-    request = Request()
-    header = request.header
-    body = request.body	  
-    header.routing_id = 4
-    header.originator = "zero"
-    job_op = body.job_op
-    job_op.action = 4
+    request = Request.getDefaultInstance()
+    #header = request.header
+    #body = request.body	  
+    #header.routing_id = 4
+    #header.originator = "zero"
+    #job_op = body.job_op
+    #job_op.action = 4
     
     group = NioEventLoopGroup()
     
     try:
-      make_request(group, host, port, request)
+      bootstrap = Bootstrap().group(group).channel(NioSocketChannel).handler(SSLInitializer()).option(ChannelOption.TCP_NODELAY, True).option(ChannelOption.ALLOCATOR, PooledByteBufAllocator.DEFAULT)
+      channel = bootstrap.connect(host, port).sync().channel()
+      channel.writeAndFlush(request).sync()
+      channel.read()
     finally:
-        
+      print "Shutting down"
+      group.shutdown()
+
     
