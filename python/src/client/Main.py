46e0036dc030bd13d66c415c5a1da611a8540535
anetfreak <anetfreak@gmail.com>
2014-04-05 17:12:09 -0700

    Some awesome client side progress and features... :D

--- python/src/client/Main.py
+++ python/src/client/Main.py
@@ -4,10 +4,38 @@ from PyClient import PyClient
 
 if __name__ == '__main__':
   import sys
-  if len(sys.argv) != 3:
-    print "Usage: python Main.py <host> <port>"
-  else:
-    host = sys.argv[1]
-    port = int(sys.argv[2])
-    print "Trying to connect to " + host + "..."
-    PyClient().run(host, port)
+  
+  #Display the menu to the user. Perform the necessary action as per his choice.. 
+  running = True
+  while running:
+      print "\n   ====================="
+      print "   Welcome to Open MOOC!"
+      print "   =====================\n"
+      print "Choose one of the options below:"
+      print "1. Basic Ping Request to this MOOC"
+      print "2. List Courses on this MOOC"
+      print "3. Get Description for a course"
+      print "4. Get More Courses from other MOOC's"
+      print "5. Quit"
+      
+      choice = int(input("\nYour Option -> "))
+      if choice == 1:
+          #Sending Ping Request to Server
+          requestType = "ping"
+          host = str(input("Enter host: "))
+          port = int(input("Enter port: "))
+          PyClient().chooseOperation(requestType, host, port)
+      elif choice == 2:
+          #Sending Ping Request to Server
+          print ""  
+      elif choice == 3:
+          #Sending Ping Request to Server
+          print ""
+      elif choice == 4:
+          #Sending Ping Request to Server
+          print ""
+      elif choice == 5:
+          print "Bye!"
+          running = False
+      else:
+          print "Please choose a valid option. -> "
--- python/src/client/PyClient.py
+++ python/src/client/PyClient.py
@@ -6,33 +6,53 @@ import sys
 class PyClient():
   def __init__(self):
     self.channelFactory = SocketChannelFactory()
-    
-  def run(self, host, port):
+  
+  def chooseOperation(self, choice, host, port):
+      if choice == "ping":
+          #Form Ping request here and send it to the server.
+          request = self.formPingRequest()
+          print "Preparing to send ping request to server <" + str(host) + ":" + str(port) +">"
+          response = self.run(host, port, request)
+          self.printPingRequest(response)
+      elif choice == "listcourses":
+          #Form Job request here and send it to the server.
+          print "Preparing to send job request to list courses to server <" + str(host) + ":" + str(port) +">"
+      elif choice == "getdescription":
+          #Form Job request here and send it to the server.
+          print "Preparing to send job request to get course description to server <" + str(host) + ":" + str(port) +">"
+      elif choice == "getmorecourses":
+          #Form Job request here and send it to the server. The server should take care of sending this to the external MOOC's
+          print "Preparing to send job request to get other MOOC's courses to server <" + str(host) + ":" + str(port) +">"
+  
+  def formPingRequest(self):
+      request = Request()
+      header = request.header
+      body = request.body
+      
+      header.routing_id = 2
+      header.originator = "zero"
+      ping = body.ping
+      ping.number = 4
+      ping.tag = "zero"
+      return request
+  
+  def printPingRequest(self, resp):
+      print "\n==Response Received from Server==\n"
+      print "RoutingID - " + str(resp.header.routing_id)
+      print "Originator - " + str(resp.header.originator)
+      print "Ping Number - " + str(resp.body.ping.number)
+      print "Ping Tag - " + str(resp.body.ping.tag)
+                      
+  def run(self, host, port, request):
     self.channel = self.channelFactory.openChannel(host, port)
     while self.channel.connected:
       print "Channel Connected..."
       try:
-          request = Request()
-          header = request.header
-          body = request.body
-          
-          header.routing_id = 2
-          header.originator = "zero"
-          ping = body.ping
-          ping.number = 4
-          ping.tag = "zero"
-    
           self.channel.write(request.SerializeToString())
           resp = Request()
           resp.ParseFromString(self.channel.read())
-          print "\n==Response Received from Server==\n"
-          print "RoutingID - " + str(resp.header.routing_id)
-          print "Originator - " + str(resp.header.originator)
-          print "Ping Number - " + str(resp.body.ping.number)
-          print "Ping Tag - " + str(resp.body.ping.tag)
-          
+          return resp
       except:
         print sys.exc_info()[0]
-          
       finally:
         self.channel.close()  
