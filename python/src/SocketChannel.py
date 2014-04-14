db4557bbb563ed3587c21020dadd4fbff2f508c7
anetfreak <anetfreak@gmail.com>
2014-04-14 14:44:39 -0700

    removing the comments

--- python/src/SocketChannel.py
+++ python/src/SocketChannel.py
@@ -2,30 +2,19 @@ import socket
 import struct
 
 class SocketChannelFactory():
-  '''
-  Provides method to create channel connection.
-  '''
   def openChannel(self, host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
     return SocketChannel(sock)
 
 class SocketChannel():
-  '''
-  SocketChannel provides an abstraction layer above the 
-  underlying socket, which sends and receives messages framed
-  by their length as 4 bytes in Big Endian.
-  '''
+  
   def __init__(self, sock):
     self.sock = sock
     self.connected = True
   
   def write(self, byteStream):
-    '''
-    Write a byte stream message to the channel.
-    The message will be prepended by its length packed
-    in 4 bytes in Big Endian.
-    '''
+    
     streamLen = struct.pack('>L', len(byteStream))
     framedStream = streamLen + byteStream
     try:
@@ -35,11 +24,7 @@ class SocketChannel():
       raise Exception("socket send fail, close")
       
   def read(self):
-    '''
-    Read a byte stream message prepended by its length
-    in 4 bytes in Big Endian from channel.
-    The message content is returned.
-    '''
+    
     lenField = self.readnbytes(4)
     length = struct.unpack('>L', lenField)[0]
     byteStream = self.readnbytes(length)
